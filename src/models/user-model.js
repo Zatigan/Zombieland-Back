09084bf6f2bf8167ff57ac91c8da6b54323b06c4
BrunoJB <bruno.jean-baptiste@oclock.school>
2024-10-29 17:10:37 +0100

    feat: models attractions + user

---
+++ src/models/user-model.js
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "./sequelize-client.js";
+
+export class User extends Model {}
+
+User.init ({
+  firstname: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  profil_image: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+}, {
+  tableName: 'user',
+  timestamps: false,
+});
+
+module.exports = User;

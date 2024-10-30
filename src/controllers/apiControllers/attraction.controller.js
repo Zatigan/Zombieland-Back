f3253e23edf9540be8339fce37a7b57dd57cc82a
Mickaël-Goua <micke.goua@gmail.com>
2024-10-30 10:57:02 +0100

    feat: get one attraction

--- src/controllers/apiControllers/attraction.controller.js
+++ src/controllers/apiControllers/attraction.controller.js
@@ -4,3 +4,19 @@ export async function getAllAttractions(req,res) {
   const attractions = await Attraction.findAll({ include: "categories" });
   res.json(attractions);
 };
+
+export async function getOneAttraction(req, res) {
+  const attractionId = parseInt(req.params.id)
+
+  if (isNaN(attractionId)) {
+    return res.status(404).json({ error: "Attraction not found. Please verify the provided id"})
+  }
+
+  const attraction = await Attraction.findByPk(attractionId);
+
+  if (! attraction) {
+    return res.status(404).json({ error: "Attraction not found. Please verify the provided id"})
+  }
+
+  res.json(attraction)
+}

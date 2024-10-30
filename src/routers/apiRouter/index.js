5e9d92da80da0990964bee5bba4f614a98c7b489
Laura-Lacoste <lacostelaura@live.fr>
2024-10-30 16:35:06 +0100

    feat : create category page

--- src/routers/apiRouter/index.js
+++ src/routers/apiRouter/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { router as attractionsRouter} from "./attraction.router.js"
+import { router as categoryRouter} from "./category.router.js"
 
 export const router = Router();
 
+router.use(categoryRouter);
 router.use(attractionsRouter);

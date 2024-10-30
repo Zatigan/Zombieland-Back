aaa16ef7dcf6e1ce64b403685f7f18454be78ad2
Zatigan <joe.azerty@tutanota.de>
2024-10-30 18:45:41 +0100

    Feat: basis for authentification ok ?

--- src/routers/apiRouter/attraction.router.js
+++ src/routers/apiRouter/attraction.router.js
@@ -9,3 +9,4 @@ router.get("/attractions", cw(attractionsController.getAllAttractions));
 router.get('/attractions/random', cw(attractionsController.getThreeRandomAttractions));
 router.get("/attractions/:id", cw(attractionsController.getOneAttraction));
 router.get("/attractions/category/:id", cw(attractionsController.getAttractionByCategory));
+/*router.get("attractions/login, cw(je-sais-pas-encore.getLoggedIn));*/

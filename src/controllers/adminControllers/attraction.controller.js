93cef6482b9a7584af7bfd2c0f99bf42d2faf612
Laura-Lacoste <lacostelaura@live.fr>
2024-11-06 14:23:32 +0100

    Feat: attractions complete bo

--- src/controllers/adminControllers/attraction.controller.js
+++ src/controllers/adminControllers/attraction.controller.js
@@ -8,6 +8,7 @@ export async function attractionPage(req,res) {
 
 export async function getOneAttraction(req, res) {
   const attractionId = parseInt(req.params.id)
+
   const attraction = await Attraction.findByPk(attractionId, {
     include: "categories"
   });
@@ -15,7 +16,46 @@ export async function getOneAttraction(req, res) {
 };
 
 export async function updateAttraction(req, res){
-  res.redirect('/admin/attractions?success=true');
+
+  const attractionId = parseInt(req.params.id);
+console.log(attractionId)
+  const attraction = await Attraction.findByPk(attractionId);
+
+console.log(req.body)
+  const categoryselected = req.body.category
+  const categoryBdd = await Category.findOne(({ where: { name: categoryselected } }));
+
+
+  const image = req.files['image'] ? `/img/${req.files['image'][0].filename}` : attraction.image;
+  const caroussel1 = req.files['caroussel1'] ? `/img/${req.files['caroussel1'][0].filename}` : attraction.caroussel1;
+  const caroussel2 = req.files['caroussel2'] ? `/img/${req.files['caroussel2'][0].filename}` : attraction.caroussel2;
+  const caroussel3 = req.files['caroussel3'] ? `/img/${req.files['caroussel3'][0].filename}` : attraction.caroussel3;
+
+  
+  const {name, description_short, description_long, opening_time, closing_time, disable_access, weather_hazard, height_restriction, health_hazard} = req.body
+
+
+ 
+  const attractionData = {
+  name,
+  image, 
+  description_short,
+  description_long,
+  opening_time,
+  closing_time,
+  disable_access,
+  weather_hazard,
+  height_restriction,
+  health_hazard,
+  caroussel1,
+  caroussel2,
+  caroussel3
+ }
+
+ const updatedAttraction = await attraction.update(attractionData);
+ await updatedAttraction.setCategories(categoryBdd);
+ res.redirect('/admin/attractions?success=true');
+
 }
 
 export async function addAttractionPage(req, res) {
@@ -26,7 +66,6 @@ export async function addAttraction(req, res) {
   const categoryselected = req.body.category
   const categoryBdd = await Category.findOne(({ where: { name: categoryselected } }));
 
-  const categoryId = categoryBdd.id;
   
   const image = req.files['image'][0].filename;
   const imageSaved = `/img/${image}`;

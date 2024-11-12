dbf0dfcb9e8022b3df0a1eb3f63593f8665a88c6
Laura-Lacoste <lacostelaura@live.fr>
2024-11-12 11:54:22 +0100

    feat: update reservation

--- src/routers/apiRouter/user.router.js
+++ src/routers/apiRouter/user.router.js
@@ -10,8 +10,7 @@ router.delete("/profile/delete", userLoggedIn, cw(userRouter.delProfile));
 router.get("/profile", userLoggedIn, cw(userRouter.myProfile));
 router.get("/profile/reservation",userLoggedIn, cw(userRouter.reservationByProfile))
 router.delete("/profile/reservation/del",userLoggedIn, cw(userRouter.delReservationByProfile));
-
+router.patch("/profile/reservation/update",userLoggedIn, cw(userRouter.updateReservationByProfile));
 /* router.get("/user", cw(userRouter.getAllUsers));
 router.get("/user/:id", cw(userRouter.getOneUser)); */
 router.patch("/profile/update",userLoggedIn, cw(userRouter.updateUser));
-

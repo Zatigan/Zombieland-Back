0177d89a92408440b122962e56fe6d288a3ca85a
Mickaël-Goua <micke.goua@gmail.com>
2024-11-15 12:19:57 +0100

    fix middleware userLoggedIn and feat bo admin reservation

---
+++ src/routers/adminRouter/reservation.router.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as reservationController from '../../controllers/adminControllers/reservation.controller.js';
+import { isAdmin } from "../../middleware/isAdmin.js";
+import { controllerWrapper as cw } from "./controller.wrapper.js";
+
+export const router = Router();
+
+router.get("/reservations", cw(reservationController.reservationPage))
+router.get("/reservations", cw (reservationController.getOneReservation))

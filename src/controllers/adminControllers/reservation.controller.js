1375ddce252b53860ae562632644eb1c34665ac2
Mickaël-Goua <micke.goua@gmail.com>
2024-11-18 14:10:56 +0100

    reservation page admin ok

--- src/controllers/adminControllers/reservation.controller.js
+++ src/controllers/adminControllers/reservation.controller.js
@@ -27,8 +27,19 @@ export async function updateReservation(req, res) {
   const reservation = await Reservation.findByPk(reservationId)
 
 const { ref_number, date, ticket, total_price }= req.body
-const cost = ticket*60
 
+// Vérification de la validité de la date
+const currentDate = new Date();
+const reservationDate = new Date(date);
+
+if (reservationDate < currentDate) {
+  return res.render("reservation", {
+    reservation,
+    errorMessageTest: "La date de réservation doit être dans le futur.",
+  });
+}
+
+const cost = ticket*60
 
 await reservation.update({
   ref_number: ref_number || reservation.ref_number,
@@ -45,4 +56,57 @@ export async function deleteReservation(req, res) {
   await Reservation.destroy({ where: { id: reservationId }});
 
   res.status(201).redirect('/admin/reservations?success=true')
+}
+
+export async function addReservationPage(req, res) {
+  res.render("newReservation")
+}
+
+export async function addReservation(req, res) {
+
+  const { email, ref_number, date, ticket, total_price } = req.body;
+  
+  // On vérifie qu'aucun champ obligatoire n'est manquant / vide
+  if (!email || !date || !ticket) {
+    res.status(400).json(({ message: 'Merci de remplir tous les champs obligatoires'}));
+    return; 
+  }
+
+  // Vérification de la validité de la date
+  const currentDate = new Date();
+  const reservationDate = new Date(date);
+
+  if (reservationDate < currentDate) {
+    return res.render("newReservation", {
+      errorMessage: "La date de réservation doit être dans le futur.",
+    });
+  }
+
+
+  // On recherche un utilisateur
+  const user = await User.findOne({ where: { email }})
+
+  if (!user) {
+    return res.status(404).send("Utilisateur non trouvé");
+  }
+  const cost = ticket*60
+
+  // Recherche la dernière réservation créée pour récupérer le numéro de référence le plus élevé
+  const lastRefReservation = await Reservation.findOne({
+    order: [['ref_number', 'DESC']],
+    attributes: ['ref_number'],
+  });
+
+  const newRefNumber = lastRefReservation.ref_number +1;
+
+  const reservationData= {
+    user_id: user.id,
+    ref_number: newRefNumber,
+    date,
+    ticket,
+    total_price: cost
+  }
+
+  await Reservation.create(reservationData)
+  res.redirect('/admin/reservations?success=true')
 }
--- src/routers/adminRouter/reservation.router.js
+++ src/routers/adminRouter/reservation.router.js
@@ -5,7 +5,9 @@ import { controllerWrapper as cw } from "./controller.wrapper.js";
 
 export const router = Router();
 
-router.get("/reservations", cw(reservationController.reservationPage))
-router.get("/reservations/:id", cw (reservationController.getOneReservation))
-router.post("/reservations/:id", cw(reservationController.updateReservation))
-router.post("/reservations/del/:id", cw(reservationController.deleteReservation))
+router.get("/reservations",isAdmin, cw(reservationController.reservationPage))
+router.get("/reservations/add",isAdmin, cw(reservationController.addReservationPage))
+router.post("/reservations/add",isAdmin, cw(reservationController.addReservation))
+router.get("/reservations/:id",isAdmin, cw (reservationController.getOneReservation))
+router.post("/reservations/:id",isAdmin, cw(reservationController.updateReservation))
+router.post("/reservations/del/:id",isAdmin, cw(reservationController.deleteReservation))
